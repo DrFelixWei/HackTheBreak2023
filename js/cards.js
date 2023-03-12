e9158f251a9b9a96c88d15e27ac1e11bdc9839b0
Felix Wei <felixwei7@gmail.com>
2023-03-11 23:14:21 -0800

    added card dynamic loading 2nd level

--- js/cards.js
+++ js/cards.js
@@ -1,7 +1,20 @@
 //  loads the page with all cards regardless of tags
 $(document).ready(function() {
-  var numCards = $('#index_container > .card').length;
-  for (var i = 1; i <= numCards; i++) {
+
+  
+  var numberOfCards = $('#index_container > .card').length;
+  numberOfCards = 6;
+  console.log(numberOfCards);
+
+  // creates placeholders for cards
+  let cardsPlaceholder = "";
+  for (let n = 1; n <= numberOfCards; n++) {
+    cardsPlaceholder += "<a id=\"card" + n + "\" class=\"card\"></a>"
+  }
+  document.getElementById("index_container").innerHTML = cardsPlaceholder;
+
+  // subs in each cardx.html for the placeholder
+  for (var i = 1; i <= numberOfCards; i++) {
     $.ajax({
       url: '/cards/card' + i + '.html',
       success: function(data) {
@@ -12,3 +25,15 @@ $(document).ready(function() {
   }
   console.log("cards.js ran successfully.");
 });
+
+
+// for tags
+// document.querySelector(".tagButton").addEventListener("click", function (e) {
+//    event.stopPropagation();
+  
+  
+  // ajaxGET("/action?format=html", function (data) {
+  //     // console.log(data);
+  //     document.getElementById("action-html").innerHTML = data;
+  // });
+// });

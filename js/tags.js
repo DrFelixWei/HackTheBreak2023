1e681b7d1bd5c5075323ce26a8050098e2412ba7
Felix Wei <felixwei7@gmail.com>
2023-03-12 01:44:35 -0800

    made tagging work

--- js/tags.js
+++ js/tags.js
@@ -1,7 +1,7 @@
 // delay function to give time for placeholders to load in
 setTimeout(function() {
 
-    // Select all elements with class="tagButton"
+  // Select all elements with class="tagButton"
   const buttons = document.querySelectorAll('.tagButton');
 
   // Loop through all buttons and add event listener to each one
@@ -11,50 +11,46 @@ setTimeout(function() {
 
   // Handle click event
   function handleClick(event) {
-    var numberOfCards = 0;
-    
+    const cardUrls = [];
+
+    // Find card files that contain the clicked element's id
     $.ajax({
       url: '/cards/',
       async: false,
       success: function(data) {
         $(data).find('a[href$=".html"]').each(function(){
-          let url = $(this).attr('href');
+          const url = $(this).attr('href');
           $.ajax({
-          url: url,
-          async: false,
-          success: function(data){
-            if($(data).find('#'+event.target.id).length){
-              numberOfCards++;
+            url: url,
+            async: false,
+            success: function(data){
+              if($(data).find('#'+event.target.id).length){
+                cardUrls.push(url);
               }
             }
           });
         });
-        }
+      }
     });
-  
+
     // creates placeholders for cards
     let cardsPlaceholder = "";
-    for (let n = 1; n <= numberOfCards; n++) {
-      cardsPlaceholder += "<a id=\"card" + n + "\" class=\"card\"></a>"
+    for (let i = 0; i < cardUrls.length; i++) {
+      cardsPlaceholder += `<a id="card${i}" class="card"></a>`;
     }
     document.getElementById("index_container").innerHTML = cardsPlaceholder;
-  
+
     // subs in each cardx.html for the placeholder
-    for (var i = 1; i <= numberOfCards; i++) {
+    for (let i = 0; i < cardUrls.length; i++) {
       $.ajax({
-        url: '/cards/card' + i + '.html',
+        url: cardUrls[i],
         success: function(data) {
-          $('#card' + this.index).html(data);
-        },
-        index: i
+          $(`#card${i}`).html(data);
+        }
       });
     }
+
     console.log("cards.js ran successfully.");
-
-
   }
 
 }, 3000);
-
-
-

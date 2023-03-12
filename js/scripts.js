c9c53a03ca04decd7f98489f97b5d17bd81656d2
Felix Wei <felixwei7@gmail.com>
2023-03-11 20:38:46 -0800

    playing around with linkedin scraper

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,13 @@
 const url = 'https://www.linkedin.com/search/results/learning/?difficultyLevel=%5B%22Advanced%22%5D&keywords=programming&origin=FACETED_SEARCH&sid=dt4';
 
-axios.get(url)
+axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
   .then(response => {
-    const $ = cheerio.load(response.data);
-    const results = [];
-    $('.search-results__result-item').each((i, elem) => {
-      const title = $(elem).find('.result-lockup__title').text().trim();
-      const author = $(elem).find('.result-lockup__metadata-author').text().trim();
-      const level = $(elem).find('.result-lockup__metadata-level').text().trim();
-      const duration = $(elem).find('.result-lockup__metadata-duration').text().trim();
-      const image = $(elem).find('.result-lockup__image').attr('src');
-      const url = $(elem).find('.result-lockup__link').attr('href');
-      results.push({ title, author, level, duration, image, url });
-    });
-    const markup = results.map(result => `
-      <div class="course">
-        <img src="${result.image}" alt="${result.title}">
-        <div>
-          <h2>${result.title}</h2>
-          <p>Author: ${result.author}</p>
-          <p>Difficulty Level: ${result.level}</p>
-          <p>Duration: ${result.duration}</p>
-          <a href="${result.url}" target="_blank">View Course</a>
-        </div>
-      </div>
-    `).join('');
-    $('#results').html(markup);
+    const html = response.data.contents;
+    console.log(html);
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const resultsContainer = document.getElementById('linkedin-results');
+    const searchResults = doc.querySelectorAll('.search-results__list > li');
+    searchResults.forEach(result => resultsContainer.appendChild(result));
   })
-  .catch(error => {
-    console.log(error);
-  });
+  .catch(error => console.error(error));
